fba1af1baa14c0adf2e0c336ec3c3b5d53570372
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2023-09-18 19:50:47 +0530

    Added error handling mechanism

--- calculator.cpp
+++ calculator.cpp
@@ -1,57 +1,134 @@
-
 #include "Calculator.h"
 #include <iostream>
-#include <cmath>
+#include <stdexcept>
+#include <cassert>
 
-double Calculator::add(double a, double b) {
-    return a + b;
-}
-
-double Calculator::subtract(double a, double b) {
-    return a - b;
-}
-
-double Calculator::multiply(double a, double b) {
-    return a * b;
-}
-
-double Calculator::divide(double a, double b) {
-    if (b == 0) {
-        throw std::invalid_argument("Division by zero");
+double Calculator::add(double a, double b)
+{
+    try
+    {
+        assert(!std::isnan(a) && !std::isnan(b));
+        return a + b;
     }
-    return a / b;
-}
-
-double Calculator::percentage(double num, double percent) {
-    return (num * percent) / 100.0;
-}
-
-
-double Calculator::sqrt(double num) {
-    if (num < 0) {
-        throw std::invalid_argument("Square root of a negative number");
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in add(): " << e.what() << std::endl;
+        throw;
     }
-    return std::sqrt(num);
 }
 
-double Calculator::square(double num) {
-    return num * num;
-}
-
-double Calculator::power(double base, double exponent) {
-    return std::pow(base, exponent);
-}
-
-double Calculator::ln(double num) {
-    if (num <= 0) {
-        throw std::invalid_argument("Natural logarithm of a non-positive number");
+double Calculator::subtract(double a, double b)
+{
+    try
+    {
+        assert(!std::isnan(a) && !std::isnan(b));
+        return a - b;
     }
-    return std::log(num);
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in subtract(): " << e.what() << std::endl;
+        throw;
+    }
 }
 
-double Calculator::log(double num, double base) {
-    if (num <= 0 || base <= 0 || base == 1) {
-        throw std::invalid_argument("Logarithm with invalid arguments");
+double Calculator::multiply(double a, double b)
+{
+    try
+    {
+        assert(!std::isnan(a) && !std::isnan(b));
+        return a * b;
     }
-    return std::log(num) / std::log(base);
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in multiply(): " << e.what() << std::endl;
+        throw;
+    }
+}
+
+double Calculator::divide(double a, double b)
+{
+    try
+    {
+        assert(!std::isnan(a) && !std::isnan(b));
+        if (b == 0)
+        {
+            throw std::runtime_error("Division by zero");
+        }
+        return a / b;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in divide(): " << e.what() << std::endl;
+        throw;
+    }
+}
+
+double Calculator::percentage(double a, double b)
+{
+    try
+    {
+        assert(!std::isnan(a) && !std::isnan(b) && b != 0);
+        return (a / b) * 100.0;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in percentage(): " << e.what() << std::endl;
+        throw;
+    }
+}
+
+double Calculator::sqrt(double a)
+{
+    try
+    {
+        assert(!std::isnan(a) && a >= 0);
+        return std::sqrt(a);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in squareRoot(): " << e.what() << std::endl;
+        throw;
+    }
+}
+
+double Calculator::power(double base, double exponent)
+{
+    try
+    {
+        assert(!std::isnan(base) && !std::isnan(exponent));
+        return std::pow(base, exponent);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in power(): " << e.what() << std::endl;
+        throw;
+    }
+}
+
+double Calculator::ln(double a)
+{
+    try
+    {
+        assert(!std::isnan(a) && a > 0);
+        return std::log(a);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in naturalLog(): " << e.what() << std::endl;
+        throw;
+    }
+}
+
+double Calculator::log(double base, double a)
+{
+    try
+    {
+        assert(!std::isnan(base) && !std::isnan(a) && base > 0 && a > 0);
+        return std::log(a) / std::log(base);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error in log(): " << e.what() << std::endl;
+        throw;
+    }
 }
